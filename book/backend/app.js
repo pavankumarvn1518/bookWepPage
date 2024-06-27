eedcd2a6b172057712254dc3d3f34ec74db59733
pavankumarvn1518 <pavnkumarvn1518>
2024-06-27 17:03:24 +0530

    a few line added in app.js  related to (backend/route/cart.js)

--- book/backend/app.js
+++ book/backend/app.js
@@ -5,6 +5,8 @@ require("./conn/conn");
 const User = require("./routes/user");
 const Books = require("./routes/book");
 const Favourite = require("./routes/favourite");
+const Cart = require("./routes/cart");
+
 
 
 
@@ -15,6 +17,8 @@ app.use(express.json());
 app.use("/api/v1", User);
 app.use("/api/v1", Books);
 app.use("/api/v1", Favourite);
+app.use("/api/v1", Cart);
+
 
 
 // Test route for root URL
