bfda04c9d301a9586537279e66fca24f1a7f9378
pavankumarvn1518 <pavnkumarvn1518>
2024-07-15 23:32:04 +0530

    a simple foter code completed

--- book/frontend/src/components/Footer/Footer.jsx
+++ book/frontend/src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Footer() {
-  return (
-    <div>
-      Footer
+const Footer = () =>{
+ 
+   return <div className='bg-zinc-800 text-white px-4 py-4'>
+      <h1 className='text-2xl front-semibold  text-center'>&copy;2024, made withLove By PAVAN NAIK </h1>
     </div>
-  )
+  
 }
 
 export default Footer
