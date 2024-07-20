c777a7c305094f0db9b42142923659e4e729c0d0
pavankumarvn1518 <pavnkumarvn1518>
2024-07-20 13:50:36 +0530

    routes link added on App.jsx

--- book/frontend/src/App.jsx
+++ book/frontend/src/App.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer/Footer';
+import {BrowserRouter as Router, Routes,Route  } from "react-router-dom";
+import AllBooks from './pages/AllBooks';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import Cart from './pages/Cart';
+import Profile from './pages/Profile';
+
 
 const App = () => {
   return (
     <div>
-     <Navbar />
-    <Home />
-    <Footer />
-      
+      <Router > 
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={ <Home />} />
+        <Route  path="/all-books" element={ <AllBooks />} />
+        <Route  path="/Cart" element={ <Cart />} />
+        <Route  path="/Profile" element={ <Profile />} />
+        <Route  path="/SignUp" element={ <SignUp />} />
+        <Route  path="/LogIn" element={ <LogIn />} />
+      </Routes>
+      <Footer />
+      </Router>
     </div>
-  )
-}
+  );
+};
 
 export default App
